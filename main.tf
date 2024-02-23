35fb1fbe8dc7ddeff5e6d1f6b6eff3c4ef9aa43b
Shivam Kumar <shivam.sk3002@gmail.com>
2024-02-23 15:22:55 +0530

    updates

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "phrasal-acolyte-415205" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
